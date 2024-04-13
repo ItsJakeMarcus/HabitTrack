53c1a71f19108b7b9a507ec1a4aa915c8f257a7e
Jake Maidment <jmaidmentdev@gmail.com>
2024-04-13 19:16:33 +0100

    Initial Commit

---
+++ HabitTrack/HabitTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackApp.swift
+//  HabitTrack
+//
+//  Created by Jake Maidment on 13/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
